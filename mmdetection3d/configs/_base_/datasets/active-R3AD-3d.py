fc68e922665fe0cbca93416abd71e66a45c2bd3b
pioneer-innovation <54060693+pioneer-innovation@users.noreply.github.com>
2022-01-17 16:50:02 +0800

    Add files via upload

---
+++ mmdetection3d/configs/_base_/datasets/active-R3AD-3d.py
@@ -0,0 +1,91 @@
+dataset_type = 'R3ADDataset'
+data_root = '/media/datc/experiment/R3AD/data/'
+class_names = ('table', 'chair', 'sofa', 'bed', 'cabinet', 'television', 'lamp',
+               'shelf', 'bottle', 'cup', 'book', 'dustbin')
+train_pipeline = [
+    dict(
+        type='R3ADLoadPointsFromFile',
+        coord_type='DEPTH',
+        shift_height=True,
+        load_dim=6,
+        use_dim=[0, 1, 2, 3, 4, 5]),
+    dict(type='LoadAnnotations3D'),
+    # dict(type='Open3dShow'),
+    dict(
+        type='RandomFlip3D',
+        sync_2d=False,
+        flip_ratio_bev_horizontal=0.5,
+    ),
+    dict(
+        type='GlobalRotScaleTrans',
+        rot_range=[-0.523599, 0.523599],
+        scale_ratio_range=[0.85, 1.15],
+        shift_height=True),
+    dict(type='IndoorPointSample', num_points=20000),
+    dict(type='DefaultFormatBundle3D', class_names=class_names),
+    dict(type='Collect3D', keys=['points', 'gt_bboxes_3d', 'gt_labels_3d'])
+]
+test_pipeline = [
+    dict(
+        type='R3ADLoadPointsFromFile',
+        coord_type='DEPTH',
+        shift_height=True,
+        load_dim=6,
+        use_dim=[0, 1, 2, 3, 4, 5]),
+    dict(
+        type='MultiScaleFlipAug3D',
+        img_scale=(1333, 800),
+        pts_scale_ratio=1,
+        flip=False,
+        transforms=[
+            dict(
+                type='GlobalRotScaleTrans',
+                rot_range=[0, 0],
+                scale_ratio_range=[1., 1.],
+                translation_std=[0, 0, 0]),
+            dict(
+                type='RandomFlip3D',
+                sync_2d=False,
+                flip_ratio_bev_horizontal=0.5,
+            ),
+            dict(type='IndoorPointSample', num_points=20000),
+            dict(
+                type='DefaultFormatBundle3D',
+                class_names=class_names,
+                with_label=False),
+            dict(type='Collect3D', keys=['points'])
+        ])
+]
+
+data = dict(
+    samples_per_gpu=16,
+    workers_per_gpu=4,
+    train=dict(
+        type='RepeatDataset',
+        times=5,
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=data_root + 'active_R3AD_infos_train.pkl',
+            pipeline=train_pipeline,
+            classes=class_names,
+            filter_empty_gt=False,
+            # we use box_type_3d='LiDAR' in kitti and nuscenes dataset
+            # and box_type_3d='Depth' in sunrgbd and scannet dataset.
+            box_type_3d='Depth')),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=data_root + 'active_R3AD_infos_val.pkl',
+        pipeline=test_pipeline,
+        classes=class_names,
+        test_mode=True,
+        box_type_3d='Depth'),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=data_root + 'active_R3AD_infos_val.pkl',
+        pipeline=test_pipeline,
+        classes=class_names,
+        test_mode=True,
+        box_type_3d='Depth'))
---
+++ mmdetection3d/configs/votenet/votenet_R3AD.py
@@ -0,0 +1,31 @@
+_base_ = [
+    '../_base_/datasets/R3AD-3d.py', '../_base_/models/votenet.py',
+    '../_base_/schedules/schedule_3x.py', '../_base_/default_runtime.py'
+]
+# model settings
+model = dict(
+    backbone=dict(
+        in_channels=7,
+    ),
+    bbox_head=dict(
+        num_classes=12,
+        bbox_coder=dict(
+            type='PartialBinBasedBBoxCoder',
+            num_sizes=12,
+            num_dir_bins=12,
+            with_rot=True,
+            mean_sizes=[
+                [1.1, 0.5, 0.5],
+                [0.7, 1.2, 0.7],
+                [1.8, 1.0, 1.0],
+                [2.3, 2.3, 0.6],
+                [1.6, 1.0, 1.0],
+                [0.6, 1.3, 0.7],
+                [0.2, 0.4, 1.7],
+                [0.7, 0.4, 1.6],
+                [0.1, 0.1, 0.2],
+                [0.1, 0.1, 0.1],
+                [0.2, 0.2, 0.3],
+                [0.4, 0.4, 0.8]
+            ]),
+    ))
